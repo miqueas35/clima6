90e6f9b785c24c533646363baa8f50eb7ef2f80a
unknown <Dan1el-aguilar>
2022-08-17 18:39:56 -0300

    add provoder to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
-
+import { DataClima } from './providers/DataClima'
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
+    <DataClima>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </DataClima>
   );
 }
 
