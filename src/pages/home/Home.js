01ff260185b87bf6b65d85b4a3437bc3792514db
alevexler <alevexler2@gmail.com>
2022-08-19 11:46:37 -0300

    create component sidebar and add http request

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useClimaContext, useClimaToggleContext } from '../../providers/DataClima';
+import { useClimaToggleContext } from '../../providers/DataClima';
 import Sidebar from '../../components/sidebar/Sidebar';
 
 const Home = () => {
-  const clima = useClimaContext();
   const setClima = useClimaToggleContext();
   const [inputCity, setInputCity] = useState();
   const getDataClima = async () => {
-    try {
-      const data = await axios('http://api.weatherapi.com/v1/search.json?key=6be8c28794924ed8a2a184922222905&q=tuc');
-      setClima(data);
-    } catch (error) {
-      console.log(error);
+    if (inputCity.length > 3) {
+      try {
+        const data = await axios(`http://api.weatherapi.com/v1/search.json?key=6be8c28794924ed8a2a184922222905&q=${inputCity}`);
+        setClima(data.data);
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
   useEffect(() => {
     getDataClima();
-  }, []);
-
-  console.log(inputCity);
+  }, [inputCity]);
 
   return (
     <div>
